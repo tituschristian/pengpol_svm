9aa8bdd51c19c118767d01767a6a39624dcbc989
tituschristian <tituschristian98@gmail.com>
2018-11-08 16:02:33 +0700

    sequential learning

--- PengpolSVM.py
+++ PengpolSVM.py
@@ -1,28 +1,54 @@
 
 selectedKernel = 0
-degree = 0
-constanta = 0
+degree = 2
+constanta = 1
 thou = 0
-lamda = 0
+lamda = 3
+gamma = 0.05
 
 # Data
-banyakFitur = 0
-banyakData = 0
-arrayData = []
+banyakFitur = 2
+banyakData = 4
+'''arrayData = [
+  [60,  165],
+  [70,  160],
+  [80,  165],
+  [100, 155],
+  [40,  175]]'''
+arrayData = [
+  [1,    1],
+  [1,   -1],
+  [-1,   1],
+  [-1,  -1]]
+'''arrayKelas = [
+  1, 1, 1, -1, -1
+]'''
+arrayKelas = [
+  -1, 1, 1, -1
+]
 arrayD = []
 arrayK = []
 
 def setDegree():
 	global degree
 	degree = int(input("Input degree: "))
-	
+
 def setConstanta():
 	global constanta
-	constanta = int(input("Input constanta: "))
+	while True:
+		constanta = int(input("Input constanta: "))
+		if constanta < 1:
+			print("Masukkan constanta lebih dari 1")
+		else:
+			break
 
 def setLamda():
 	global lamda
 	lamda = int(input("Input lambda: "))
+
+def setGamma():
+	global gamma
+	gamma = float(input("Input gamma: "))
 
 def setArrayData():
 	global arrayData
@@ -30,6 +56,12 @@ def setArrayData():
 	global banyakFitur
 	banyakFitur = int(input("Masukkan banyak fitur: "))
 	banyakData = int(input("Masukkan banyak data: "))
+	print("Silahkan masukkan data setiap baris")
+
+	for i in range(banyakData):
+		print("Fitur ", (i+1), end='|')
+
+	print("Kelas / Kategori")
 	for i in range(banyakData):
 		row = [int(x) for x in input().split()]
 		arrayData.append(row)
@@ -37,14 +69,21 @@ def setArrayData():
 def printArrayData():
 	for i in range(len(arrayData)):
 		for j in range(len(arrayData[i])):
-			print(arrayData[i][j], " ", end='')
+			print(arrayData[i][j], end=' ')
+		print("")
+	print("")
+
+def printArrayD():
+	for i in range(len(arrayD)):
+		for j in range(len(arrayD[i])):
+			print(arrayD[i][j], end=' ')
 		print("")
 	print("")
 
 def linier(x, y):
 	result = 0
 	for i in range(len(x)):
-		result = x[i] * y[i]
+		result += x[i] * y[i]
 	return result
 			
 def polinomialDegreeD(x, y):
@@ -53,19 +92,26 @@ def polinomialDegreeD(x, y):
 	
 	result = 0
 	for i in range(len(x)):
-		result = x[i] * y[i]
+		result += x[i] * y[i]
+	result = result ** degree
 	return result
 	
 def polinomialDegreeUpToD(x, y):
 	if (degree == 0):
 		setDegree()
-	if (constanta == 0):
+	if (constanta < 1):
 		setConstanta()
 	
 	result = 0
 	for i in range(len(x)):
-		result = x[i] * y[i]
+		result += x[i] * y[i]
+		#print(result, "->", end='')
+	result = result + constanta
+	#print("result plus constanta", result)
+	result = result ** degree
+	#print("result ", result)
 	return result
+
 '''
 def setSelectedKernel():
 	print("1. Linier")
@@ -84,11 +130,56 @@ def calculateKernel()
 	else:
 '''		
 
+def calculateArrayD():
+	global arrayD
+	for i in range(banyakData):
+		row = []
+		for j in range(banyakData):
+			#print("data yang digunakan ", arrayData[i], "dan ", arrayData[j])
+			row.append( arrayKelas[i] * arrayKelas[j] 
+			* ( polinomialDegreeUpToD(arrayData[i], arrayData[j]) + (lamda**2) ))
+		print(row)
+		arrayD.append(row)
+
+
 def seqLearning():
-	ai = 0
-	a = 0
-	return 0
+	ai = [0.0 for i in range(banyakData)]
+	calculateArrayD()
+	dAi = [0.0 for i in range(banyakData)]
+	newAi = [0.0 for i in range(banyakData)]
+	while True:
+		ei = [0.0 for i in range(banyakData)]
+		for i in range(banyakData):
+			#print(ei[i] , "===>")
+			for j in range(banyakData):
+				#print(ai[i] , )
+				ei[i] += (ai[j] * arrayD[i][j])
+				#print(ai[i], "*", arrayD[i][j], "=", ei[i] , "-> ", end='')
+			#print("Ei %.6f" %ei[i])
+			dAi[i] = min( max(gamma * (1 - ei[i]), -1 * ai[i] ), (constanta - ai[i]) )
+			#print("delta Ai %.6f" %dAi[i])
+			newAi[i] = ai[i] + dAi[i]
+			#print("new Ai %.6f" %newAi[i])
+		
+		#print("cek")
+		count = 0
+		for i in range(banyakData):
+			if dAi[i] <= 0.00001:
+				count += 1
+			ai[i] = newAi[i]
+		
+		if count == banyakData:
+			break
+	
+	for i in range(len(ai)):
+		print("alpha ", (i+1), "= %.3f" %ai[i])
+		
+	return ai
+
 	
 #print(linier( [2], [2] ) )
-setArrayData()
+#setArrayData()
 printArrayData()
+seqLearning()
+#calculateArrayD()
+#printArrayD()
